f51a440e4f9ac50a1a4aa9438d552b26186a09d1
VALDEZ R. <renzv753@gmail.com>
2024-09-25 15:20:55 +0800

    Update ACT.2 VALDEZ R.CS2C.js

--- ACT.2 VALDEZ R.CS2C.js
+++ ACT.2 VALDEZ R.CS2C.js
@@ -8,7 +8,6 @@ class Book:
     def display_book(self):
         print(f"Title: {self.title}, Author: {self.author}, Borrower: {self.borrower}, Status: {self.status}")
 
-# Create book objects
 book1 = Book("1984", "George Orwell", "Alice", "Borrowed")
 book2 = Book("To Kill a Mockingbird", "Harper Lee", "Charlie", "Borrowed")
 book3 = Book("The Great Gatsby", "F. Scott Fitzgerald", "", "Available")
@@ -20,17 +19,14 @@ book8 = Book("Crime and Punishment", "Fyodor Dostoevsky", "Eve", "Borrowed")
 book9 = Book("The Lord of the Rings", "J.R.R. Tolkien", "Charlie", "Borrowed")
 book10 = Book("The Hobbit", "J.R.R. Tolkien", "Charlie", "Borrowed")
 
-# Display information for books borrowed by Alice
 print("Books borrowed by Alice:")
 for book in [book1, book4]:
     book.display_book()
 
-# Display information for the available book
 print("\nAvailable book:")
 for book in [book3]:
     book.display_book()
 
-# Display information for books written by J.R.R. Tolkien and Jane Austen
 print("\nBooks written by J.R.R. Tolkien and Jane Austen:")
 for book in [book9, book10, book5]:
     book.display_book()
